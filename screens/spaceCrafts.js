7f1d655b9edf922ae1e9c27b5e8cb292ab6447b0
KanishkCoder <76817700+KanishkCoder@users.noreply.github.com>
2021-07-02 16:02:27 +0530

    Add files via upload

---
+++ screens/spaceCrafts.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class spaceCrafts extends Component{
+
+    render(){
+        return(
+            <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}} >
+                <Text>Space Crafts Screen</Text>
+            </View>
+        )
+    }
+}
